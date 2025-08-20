504ce8bcacb350367e97b5e0551a3a61cfa5fe44
Soham Raddi <soham.raddi@gmail.com>
2025-08-20 18:50:57 +0530

    included sprints

--- session.py
+++ session.py
@@ -1,12 +1,12 @@
 import fastf1
 import fastf1.plotting as f1plot
 from fastf1.core import DataNotLoadedError
-fastf1.Cache.enable_cache('./f1_analysis/cache') 
+fastf1.Cache.enable_cache('C:\\Users\\Soham\\Codes\\github repos\\f1_analysis\\cache')
 
 def main():
     year = int(input("Enter year (e.g., 2021): "))
     gp_name = input("Enter Grand Prix name (e.g., Bahrain): ").strip()
-    session_type = input("Enter session type (FP1, FP2, FP3, Q, R): ").strip().upper()
+    session_type = input("Enter session type (FP1, FP2, FP3, SQ, S, Q, R): ").strip().upper()
 
     print(f"\nLoading {year} {gp_name} GP - {session_type}...\n")
     session = fastf1.get_session(year, gp_name, session_type)
@@ -21,7 +21,7 @@ def main():
     # Print results summary
     if session.results is not None:
         print("Session Results:")
-        print(session.results[['Abbreviation', 'Position', 'Points', 'Status']])
+        print(session.results[['Abbreviation', 'Position', 'Points', 'Status', 'Laps']])
     else:
         print("No results data available.")
     
